60ecd0bb1fc655f52ed74b0b609ef121ea0fe08e
Humam <humam.abedrabbo@hotmail.com>
2019-06-19 02:47:59 +0300

    Add SettingModel

---
+++ SmartCut/Models/SettingModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartCut.Models
+{
+    public class SettingModel
+    {
+        public int Id { get; set; }
+
+        public int MaximumCuttingLengthInCm { get; set; }
+
+        public int GramageRangePercent { get; set; }
+    }
+}
