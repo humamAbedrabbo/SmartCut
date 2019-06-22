b05060d0e11241b2bdd8f9e1c3a74efb30dfb8fb
mouinhourani <houranimouin@hotmail.com>
2019-06-22 23:33:40 +0300

    cut with out rotation

--- SmartCut/Services/Sheet.cs
+++ SmartCut/Services/Sheet.cs
@@ -12,27 +12,51 @@ namespace SmartCut.Services
 
         public int Width { get; set; }
 
-        public double LossPercent { get; set; }
+        public double LossPercent { get; set; } = 100;
 
-        public int Total { get; set; }
+        public int Total { get; set; } = 0;
 
         public Sheet(int length, int width, bool sameDirection = true)
         {
-            if (sameDirection)
-            {
-                Length = length;
-                Width = width;
-            }
-            else
-            {
-                Length = width;
-                Width = length;
-            }
+            Length = length;
+            Width = width;
+            if (!sameDirection)
+                Rotate();
+        }
+
+        void Rotate()
+        {
+            int buffer = Length;
+            Length = Width;
+            Width = buffer;
         }
 
         internal void Cut(OrderFilter filter, bool allowRotation = false)
         {
-            throw new NotImplementedException();
+            if (allowRotation)
+                Total = CutWithRotation(filter);
+            else
+                Total = CutWithOutRotation(filter);
+        }
+
+        private int CutWithOutRotation(OrderFilter filter)
+        {
+            return (Width / filter.Width) * (Length / filter.Length);
+        }
+
+        private int CutWithRotation(OrderFilter filter)
+        {
+            //check dictionary
+            if (Width == filter.Width || Length == filter.Length)
+                return CutWithOutRotation(filter);
+            if (Width == filter.Length || Length == filter.Width)
+            {
+                Rotate();
+                return CutWithOutRotation(filter);
+            }
+
+
+            //add to dictinary
         }
     }
 }
