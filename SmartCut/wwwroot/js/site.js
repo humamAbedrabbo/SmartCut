b2d6762b5475a20821b0c800f119536b67e11eec
humam <humam.abedrabbo@hotmail.com>
2019-07-06 01:30:04 +0300

    Handle ItemType Selection In Stock Item Create and Edit #5

--- SmartCut/wwwroot/js/site.js
+++ SmartCut/wwwroot/js/site.js
@@ -1,4 +1,19 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿// StockItem Create
 
-// Write your JavaScript code.
+function handleItemTypeSelection() {
+    var itemType = $("#ItemType").val();
+    var $hardness = $("#Hardness");
+    var $length = $("#Length");
+
+    //console.log($("#ItemType").val());
+    if (itemType === '0') {
+        // When Roll is selected, disable Length and Hardness Width Option
+        $hardness.children("option[value=1]").attr('disabled', "true");
+        $hardness.val(0);
+        $length.attr("readonly", "true");
+    } else {
+        // When Sheets are selected, return Length and Hardness to default state
+        $hardness.children("option[value=1]").removeAttr('disabled');
+        $length.removeAttr("readonly");
+    }
+}
